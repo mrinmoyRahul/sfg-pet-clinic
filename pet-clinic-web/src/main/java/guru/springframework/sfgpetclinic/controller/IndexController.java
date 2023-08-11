8d392fb2ff5c21033033fe2ca1c5c2c2e88bc399
mrinmoyRahul <sahamrinmoy1996@gmail.com>
2023-08-11 19:20:12 +0530

    fixed broken links. closes #45

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/IndexController.java
@@ -10,4 +10,10 @@ public class IndexController {
     {
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String findOwners()
+    {
+        return "notImplemented";
+    }
 }
